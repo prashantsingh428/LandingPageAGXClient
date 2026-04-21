46f327a3efc245d615f4fa567d305b1247f64caa
ayush31082005 <ayushchaubey19768@gmail.com>
2026-04-21 11:50:07 +0530

    Update FAQ.jsx
    routes name short

--- src/components/sections/FAQ.jsx
+++ src/components/sections/FAQ.jsx
@@ -153,7 +153,7 @@ const FAQ = () => {
           </p>
 
           <a
-            href="#consultation"
+            href="#cns"
             className="inline-block px-6 py-3 bg-primary-600 hover:bg-primary-700 text-white font-medium rounded-lg transition-colors"
           >
             Get in Touch
